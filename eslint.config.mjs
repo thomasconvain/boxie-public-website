ef8c192cfa8687e692ceaa25e1f287471e92f30e
Thomas Convain <tconvain@creditu.com>
2025-01-25 21:42:16 -0300

    corrige eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,14 +11,23 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends(
-    "next/core-web-vitals",
-    "next/typescript",
-    "plugin:prettier/recommended"
+    'next/core-web-vitals',
+    'next/typescript',
+    'plugin:prettier/recommended'
   ),
   {
-    plugins: ["prettier"],
+    files: ['**/*.{js,jsx,ts,tsx}'], // Aplica reglas a estos archivos
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      prettier: require('eslint-plugin-prettier'), // Importa el plugin directamente
+    },
     rules: {
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 ];
